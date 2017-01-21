589dda1b83a27e8a5b699d0dcda2430cf60a4c67
Ben Mercer <ben@henroben.co.uk>
2017-01-21 19:01:04 +0000

    S2:L4 - initial commit

---
+++ facebook-footer-link.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: Facebook Footer Link
+ * Description: Adds a Facebook profile link to the end of posts
+ * Version: 1.0
+ * Author: Benjamin Mercer
+ *
+ **/
+
+// Exit if accessed directly
+if(!defined('ABSPATH')) {
+	exit;
+}
+?>
---
+++ readme.md
@@ -0,0 +1,1 @@
+Basic wordpress plugin - Adds a Facebook profile link to the end of posts
